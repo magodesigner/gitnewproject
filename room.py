d5ae8faea5c140541397e05fe5d93e32bd016f35
Morgan <magodesigner1@gmai.com>
2022-02-10 16:36:36 +0300

    Added .gitigonore file with IGNORE RULES

---
+++ room.py
@@ -0,0 +1,4 @@
+
+print("welcome to the new room")
+print("This room is Pretty")
+print("now you may feel freee in this room")
---
+++ rwanda.py
@@ -0,0 +1,5 @@
+print("new rwanda")
+print("This life has no balance")
+print("remover")
+print("new remover")
+
